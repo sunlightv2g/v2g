6b10744001e7d96a02506400cd2584dd88374059
sunlightv2g <sunlightv2g@gmail.com>
2019-01-17 14:35:37 +0700

    commiit

---
+++ src/main/java/com/v2g/webservice/V2gApplication.java
@@ -0,0 +1,14 @@
+package com.v2g.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2gApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2gApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/v2g/webservice/V2gApplicationTests.java
@@ -0,0 +1,17 @@
+package com.v2g.webservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class V2gApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
