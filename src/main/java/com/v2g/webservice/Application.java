20e2b0893e4521150e22ac365e2975ac294e4f9d
sunlightv2g <sunlightv2g@gmail.com>
2019-01-17 16:43:31 +0700

    commiit

---
+++ src/main/java/com/v2g/webservice/Application.java
@@ -0,0 +1,20 @@
+package com.v2g.webservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing // JPA Auditing
+@SpringBootApplication
+public class Application {
+
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-v2g/real-application.yml";
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class)
+        .properties(APPLICATION_LOCATIONS)
+        .run(args);
+	}
+}
--- src/main/java/com/v2g/webservice/web/WebRestController.java
+++ src/main/java/com/v2g/webservice/web/WebRestController.java
@@ -1,14 +1,28 @@
 package com.v2g.webservice.web;
 
+import java.util.Arrays;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
 
-	   @GetMapping("/hello")
-	    public String hello() {
-	        return "HelloWorld";
-	    }
+	private Environment env;
 	
+	@GetMapping("/hello")
+	public String hello() {
+	    return "HelloWorld";
+	}
+	
+	@GetMapping("/profile")
+	public String getProfile () {
+	    return Arrays.stream(env.getActiveProfiles())
+	            .findFirst()
+	            .orElse("");
+	}
 }
