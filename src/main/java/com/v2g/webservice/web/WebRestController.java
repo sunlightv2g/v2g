8d568c8ff7987e40ec484f37ba875e4c64523f47
sunlightv2g <sunlightv2g@gmail.com>
2019-01-18 08:17:55 +0700

    commiit

--- src/main/java/com/v2g/webservice/web/WebRestController.java
+++ src/main/java/com/v2g/webservice/web/WebRestController.java
@@ -3,6 +3,7 @@ package com.v2g.webservice.web;
 import java.util.Arrays;
 
 import org.springframework.core.env.Environment;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,16 @@ public class WebRestController {
 
 	private Environment env;
 	
+    @GetMapping("/")
+    public String main(Model model) {
+    	//model.addAttribute("posts", postsService.findAllDesc());
+        return "main";
+    }
+	
+	
 	@GetMapping("/hello")
 	public String hello() {
-	    return "HelloWorld";
+	    return "Hello V2G";
 	}
 	
 	@GetMapping("/profile")
